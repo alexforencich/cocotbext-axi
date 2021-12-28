1b6993d80de2605b10f7c096567c04c0ee7c0510
Alex Forencich <alex@alexforencich.com>
2021-12-27 17:10:37 -0800

    Use start_soon instead of fork

--- tests/axis/test_axis.py
+++ tests/axis/test_axis.py
@@ -45,7 +45,7 @@ class TB:
         self.log = logging.getLogger("cocotb.tb")
         self.log.setLevel(logging.DEBUG)
 
-        cocotb.fork(Clock(dut.clk, 2, units="ns").start())
+        cocotb.start_soon(Clock(dut.clk, 2, units="ns").start())
 
         self.source = AxiStreamSource(AxiStreamBus.from_prefix(dut, "axis"), dut.clk, dut.rst)
         self.sink = AxiStreamSink(AxiStreamBus.from_prefix(dut, "axis"), dut.clk, dut.rst)
